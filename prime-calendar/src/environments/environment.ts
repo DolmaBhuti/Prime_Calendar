816f0ab35bf7ed457f8f5af1bc2d0a3c5fe7db8a
DolmaBhuti <dbhuti95@gmail.com>
2022-06-03 15:30:03 -0400

    added userAPIBase environment variable

--- prime-calendar/src/environments/environment.ts
+++ prime-calendar/src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
-
+  production: false,
+  userAPIBase: "https://primecalendar-server.herokuapp.com/api/user"
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
