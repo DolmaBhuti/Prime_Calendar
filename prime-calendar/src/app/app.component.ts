934db3e6eae15ac6edd2399700f39d1b70529acf
github dzhao925 <github dzhao40@myseneca.ca>
2022-06-04 09:40:54 -0400

    add routes

--- prime-calendar/src/app/app.component.ts
+++ prime-calendar/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Event, NavigationStart, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Prime Calendar';
+  token: any;
+
+  constructor(private router:Router, private auth:AuthService){}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) { 
+        this.token = this.auth.readToken();
+      }
+    });
+  }
+
+  logout(){
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
