ce7b4128d5e8077bb6f9043051783361cf00c593
DolmaBhuti <dbhuti95@gmail.com>
2022-06-09 16:25:29 -0400

    testing event functions

---
+++ prime-calendar/src/app/calendar.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from './../environments/environment';
+
+//import { JwtHelperService } from '@auth0/angular-jwt';
+import Event from './Event';
+@Injectable({
+  providedIn: 'root'
+  })
+export class CalendarService {
+    constructor( private http: HttpClient) { }
+    //add calendar service (save to the mongo db) here
+    eventAdd(event: Event): Observable<any>{
+      return this.http.post<any>(`${environment.userAPIBase}/events/add`, event);
+    }
+
+}
--- prime-calendar/src/app/calendar/calendar.component.ts
+++ prime-calendar/src/app/calendar/calendar.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular'; // useful for typechecking
+import { CalendarOptions, DateSelectArg, EventAddArg } from '@fullcalendar/angular'; // useful for typechecking
 import interactionPlugin from '@fullcalendar/interaction';
-import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { CalendarService } from '../calendar.service';
+import Events from '../Event';
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
+  event:Events = {_id:"",eventTitle: "Test event", startTime:"", eventDate: new Date(), userID:"32433234"};
+  
+  constructor(private calService : CalendarService) { }
+  private calendarSub: Subscription | undefined;
+
 
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    dateClick: this.handleDateClick.bind(this), // bind is important!
+    selectable: true,
+
+    select: this.handleDateSelect.bind(this), // bind is important!
+    //select: this.handleDateSelect.bind(this), // bind is important!
     events: [
       {       
       title: 'BCH237',
@@ -24,11 +34,13 @@ export class CalendarComponent implements OnInit {
       { 
         title: 'event 2', date: '2019-04-02' }
     ],
-    eventClick: function(info) { //https://fullcalendar.io/docs/event-model
-      console.log(info.event.extendedProps['department'])
-      alert('event click! ' + info.event.extendedProps['description'])
-      info.event.setStart('2022-06-13T10:30:00');
-    }
+    eventAdd: this.handleEventAdd.bind(this)
+    // eventClick: function(info) { //https://fullcalendar.io/docs/event-model
+    //   console.log(info.event.extendedProps['department'])
+    //   alert('event click! ' + info.event.extendedProps['description'])
+    //   info.event.setStart('2022-06-13T10:30:00');
+      
+    // }
     
     // eventContent: function(arg) {
     //   console.log(arg.event.extendedProps);
@@ -40,9 +52,41 @@ export class CalendarComponent implements OnInit {
   handleDateClick(arg:any) { //TODO: create/edit/delete event form
     alert('date click! ' + arg.dateStr)  //date click! 2022-06-06
   }
+  handleDateSelect(selectInfo: DateSelectArg) {
 
-  constructor() { }
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+    calendarApi.unselect(); // clear date selection
+    
+    
+    if (title) {
+    
+      calendarApi.addEvent({
+      
+        title,
+        
+        start: selectInfo.startStr,
+        
+        end: selectInfo.endStr,
+        
+        allDay: selectInfo.allDay
+    
+      });
+    }
 
+    }
+    handleEventAdd(addInfo: EventAddArg){
+      
+      
+      this.calendarSub = this.calService.eventAdd(addInfo).subscribe(success=>{
+        //if add event was successful
+        
+      },
+      err=>{
+        //if not successful
+        
+      });
+    }
   ngOnInit(): void {
   }
 
